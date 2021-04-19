65569986cd962ab5fbf6f794ec16688737bad999
LosEagle <6146756+LosEagle@users.noreply.github.com>
2021-04-19 22:32:10 +0200

    refactor(application): move config path resolving by env variable to Config module

--- lib/freetube_updater.ex
+++ lib/freetube_updater.ex
@@ -1,37 +1,27 @@
 defmodule FreeTubeUpdater do
-  @env Mix.env()
-
-  def env, do: @env
-
   def get_download_link() do
-    config_path =
-      case FreeTubeUpdater.env() do
-        :dev -> File.cwd!() <> "/config/config.json"
-        :prod -> File.cwd!() <> "/rel/freetube_updater/config/config.json"
-      end
-
-    config = FreeTubeUpdater.Config.parse!(config_path)
-
     nightly_artifact_url = "https://api.github.com/repos/freetubeapp/freetube/actions/artifacts"
 
+    config =
+      FreeTubeUpdater.Config.get_path
+        |> FreeTubeUpdater.Config.parse!;
+
     nightly_artifacts =
-      FreeTubeUpdater.GithubApiHandler.send_authenticated_request(
-        nightly_artifact_url,
-        config["token"]
-      )
-      |> FreeTubeUpdater.GithubApiHandler.get_response_body()
-      |> get_in(["artifacts"])
+      nightly_artifact_url
+        |> FreeTubeUpdater.GithubApiHandler.send_authenticated_request(config["token"])
+        |> FreeTubeUpdater.GithubApiHandler.get_response_body()
+        |> get_in(["artifacts"])
 
     download_link =
       nightly_artifacts
-      |> FreeTubeUpdater.ArtifactParser.get_artifact(config["os"], config["format"])
-      |> get_in(["archive_download_url"])
-      |> FreeTubeUpdater.GithubApiHandler.send_authenticated_request(config["token"])
-      |> FreeTubeUpdater.GithubApiHandler.get_response_headers()
-      |> Enum.filter(fn {key, _value} -> key === "Location" end)
-      |> Enum.at(0)
-      |> Tuple.to_list()
-      |> Enum.at(1)
+        |> FreeTubeUpdater.ArtifactParser.get_artifact(config["os"], config["format"])
+        |> get_in(["archive_download_url"])
+        |> FreeTubeUpdater.GithubApiHandler.send_authenticated_request(config["token"])
+        |> FreeTubeUpdater.GithubApiHandler.get_response_headers()
+        |> Enum.filter(fn {key, _value} -> key === "Location" end)
+        |> Enum.at(0)
+        |> Tuple.to_list()
+        |> Enum.at(1)
 
     {":ok", download_link}
   end
--- lib/freetube_updater/config/config.ex
+++ lib/freetube_updater/config/config.ex
@@ -1,4 +1,15 @@
 defmodule FreeTubeUpdater.Config do
+  @env Mix.env()
+
+  def env, do: @env
+
+  def get_path do
+    case FreeTubeUpdater.Config.env() do
+      :dev -> File.cwd!() <> "/config/config.json"
+      :prod -> File.cwd!() <> "/rel/freetube_updater/config/config.json"
+    end
+  end
+
   def parse!(path) do
     File.read!(path)
     |> Poison.decode!()
