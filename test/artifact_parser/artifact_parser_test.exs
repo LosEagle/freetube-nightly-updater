d4cbe9a2c7cda512ca45d2dc85574d72d698b3eb
LosEagle <6146756+LosEagle@users.noreply.github.com>
2021-04-20 22:26:39 +0200

    test: artifact parser coverage

---
+++ test/artifact_parser/artifact_parser_test.exs
@@ -0,0 +1,28 @@
+defmodule ArtifactParserTest do
+  use ExUnit.Case
+  doctest FreeTubeUpdater
+
+  test "finds matching artifact for windows-setup configuration" do
+    artifacts =
+      File.read!(File.cwd!() <> "/test/artifact_parser/mock_artifacts.json")
+        |> Poison.decode!()
+
+    matched_artifact = FreeTubeUpdater.ArtifactParser.get_artifact(artifacts, "Win", "Setup")
+
+    assert matched_artifact["name"] === "freetube-0.12.0-nightly-724-setup-x64.exe"
+  end
+
+  test "fails when given incorrect configuration" do
+    artifacts =
+      File.read!(File.cwd!() <> "/test/artifact_parser/mock_artifacts.json")
+        |> Poison.decode!()
+
+    assert_raise(
+      FunctionClauseError,
+      ~r/no function clause matching in Regex.match/,
+      fn ->
+        FreeTubeUpdater.ArtifactParser.get_artifact(artifacts, "Win", "AppImage")
+      end
+    )
+  end
+end
