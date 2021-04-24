f496042b025b6aa0c1386ef131273e7b2661c362
LosEagle <6146756+LosEagle@users.noreply.github.com>
2021-04-24 08:50:17 +0200

    feat: add option to run command on fetched dl link

---
+++ lib/download_link/download_link.ex
@@ -0,0 +1,32 @@
+defmodule FreeTubeUpdater.DownloadLink do
+  def fetch!(config) do
+    nightly_artifact_url = "https://api.github.com/repos/freetubeapp/freetube/actions/artifacts"
+
+    nightly_artifacts =
+      nightly_artifact_url
+        |> FreeTubeUpdater.GithubApiHandler.send_authenticated_request(config["token"])
+        |> FreeTubeUpdater.GithubApiHandler.get_response_body()
+        |> get_in(["artifacts"])
+
+    download_link =
+      nightly_artifacts
+        |> FreeTubeUpdater.ArtifactParser.get_artifact(config["os"], config["format"])
+        |> get_in(["archive_download_url"])
+        |> FreeTubeUpdater.GithubApiHandler.send_authenticated_request(config["token"])
+        |> FreeTubeUpdater.GithubApiHandler.get_response_headers()
+        |> Enum.filter(fn {key, _value} -> key === "Location" end)
+        |> Enum.at(0)
+        |> Tuple.to_list()
+        |> Enum.at(1)
+
+    download_link
+  end
+
+  def pass_to_command(download_link, config) do
+    if(config["command"] !== nil and config["command"] !== "") do
+      System.cmd(config["command"], [download_link])
+    end
+
+    {:ok, download_link}
+  end
+end
--- lib/freetube_updater.ex
+++ lib/freetube_updater.ex
@@ -1,28 +1,12 @@
 defmodule FreeTubeUpdater do
-  def get_download_link!() do
-    nightly_artifact_url = "https://api.github.com/repos/freetubeapp/freetube/actions/artifacts"
-
+  def init() do
     config =
       FreeTubeUpdater.Config.get_path
-        |> FreeTubeUpdater.Config.parse!;
+        |> FreeTubeUpdater.Config.parse!
 
-    nightly_artifacts =
-      nightly_artifact_url
-        |> FreeTubeUpdater.GithubApiHandler.send_authenticated_request(config["token"])
-        |> FreeTubeUpdater.GithubApiHandler.get_response_body()
-        |> get_in(["artifacts"])
-
-    download_link =
-      nightly_artifacts
-        |> FreeTubeUpdater.ArtifactParser.get_artifact(config["os"], config["format"])
-        |> get_in(["archive_download_url"])
-        |> FreeTubeUpdater.GithubApiHandler.send_authenticated_request(config["token"])
-        |> FreeTubeUpdater.GithubApiHandler.get_response_headers()
-        |> Enum.filter(fn {key, _value} -> key === "Location" end)
-        |> Enum.at(0)
-        |> Tuple.to_list()
-        |> Enum.at(1)
-
-    download_link
+    config
+      |> FreeTubeUpdater.DownloadLink.fetch!
+      |> FreeTubeUpdater.DownloadLink.pass_to_command(config)
+      |> IO.inspect
   end
 end
